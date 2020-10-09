a9715db86067c94e2dfbded84e3685bf64851239
Garry Singh <mrgarrysingh92@gmail.com>
2020-10-09 15:04:33 +1100

    Cart Item Reducer

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -2,6 +2,7 @@ import { cartActionTypes } from "./cart.types.js";
 
 const INITIAL_STATE = {
   hidden: true,
+  cartItems: [],
 };
 
 const cartReducer = (currentState = INITIAL_STATE, action) => {
@@ -11,6 +12,12 @@ const cartReducer = (currentState = INITIAL_STATE, action) => {
         ...currentState,
         // Convert to the opposite value of the state
         hidden: !currentState.hidden,
+      };
+    case cartActionTypes.ADD_ITEM:
+      return {
+        ...currentState,
+        // Spread in existing items in our state and add the action payload at the end of the array
+        cartItems: [...currentState.cartItems, action.payload],
       };
     default:
       return currentState;
